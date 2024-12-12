6ba2a06db329fdf7540aff918138a168c71e7270
shino655 <a12205036@zokei.ac.jp>
2024-12-13 00:59:30 +0900

    Create scriot.js

---
+++ scriot.js
@@ -0,0 +1,21 @@
+body {
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f0f0f0;
+    position: relative;
+    background-image: url('background.png');
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}
+
+img {
+    max-width: 100px;
+    max-height: 100px;
+    position: absolute;
+    transition: opacity 0.5s ease-in-out;
+}
